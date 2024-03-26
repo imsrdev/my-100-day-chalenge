829288e9eb6e910a0268b2796b079aef38306120
sara <160650459+imsrdev@users.noreply.github.com>
2024-03-26 11:34:18 +0330

    Add files via upload

---
+++ day-5-Form Wave Animation/script.js
@@ -0,0 +1,11 @@
+const Labels = document.querySelectorAll(".form-control label");
+
+Labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
